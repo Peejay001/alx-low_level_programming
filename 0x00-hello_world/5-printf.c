aa944e1849ff46b8bb2c19dc4f64e85f81761e2e
“David <teminikan27@gmail.com>
2023-03-17 23:50:20 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main Prints "with proper grammar, but the outcome is
+ * main  Prints "with proper grammar, but the outcome is
  * a piece of art," followed by a new line.
- * Return: Always 0.
+ * Return: Always 0
  */
 
 int main(void)
